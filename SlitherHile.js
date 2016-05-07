dc23333f480b2abc113f417d886b0e0d54d79e84
slitherhile <otukenbilgesi@gmail.com>
2016-05-07 18:30:52 +0300

    Güncellendi

--- SlitherHile.js
+++ SlitherHile.js
@@ -1,10 +1,15 @@
 // ==UserScript==
 // @name         Slither.io Hile
+// @updateURL https://raw.githubusercontent.com/slitherhile/Slither-io-Hile/master/SlitherHile.js
 // @namespace    slither.hile
 // @version      0.0.1
-// @description  slither.io MOD
-// @author       SlitherHile
+// @description  slither.io Hile
+// @copyright 2016, SlitherHile (http://slither-hile.blogspot.com.tr/)
+// @author       slither
+// @collaborator slither
 // @match        http://slither.io/*
+// @homepageURL http://slither-hile.blogspot.com.tr/
+// @supportURL http://slither-hile.blogspot.com.tr/
 // @run-at       document-body
 // @grant        none
 // ==/UserScript==
